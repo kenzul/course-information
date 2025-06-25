fabfba6bbffd8ef849bd158cffe34a59493fc478
Jean Alvarez <blackraisondetre126@gmail.com>
2025-06-25 11:46:52 -0500

    Refactor parts into objects

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,15 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Part name={props.part1} exercises={props.exercises1}></Part>
-      <Part name={props.part2} exercises={props.exercises2}></Part>
-      <Part name={props.part3} exercises={props.exercises3}></Part>
+      <Part content={props.part1}></Part>
+      <Part content={props.part2}></Part>
+      <Part content={props.part3}></Part>
     </div>
   )
 }
 
 const Part = (props) => {
-  return <p>{props.name} {props.exercises}</p>
+  return <p>{props.content.name} {props.content.exercises}</p>
 }
 
 const Total = (props) => {
@@ -24,20 +24,15 @@ const Total = (props) => {
 
 const App = () => {
   const course = "Half Stack Application Development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const part1 = { name: "Fundamentals of React", exercises: 10 };
+  const part2 = { name: "Using props to pass data", exercises: 7 };
+  const part3 = { name: "State of a component", exercises: 14 };
 
   return (
     <div>
       <Header course={course}></Header>
-      <Content part1={part1} part2={part2} part3={part3}
-        exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}>
-      </Content>
-      <Total total={exercises1 + exercises2 + exercises3}></Total>
+      <Content part1={part1} part2={part2} part3={part3}></Content>
+      <Total total={part1.exercises + part2.exercises + part3.exercises}></Total>
     </div>
   )
 }
