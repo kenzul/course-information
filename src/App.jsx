1b08cdce4703e86eadd32fb5ab887a7620bed4df
Jean Alvarez <blackraisondetre126@gmail.com>
2025-06-26 09:24:42 -0500

    Show total number of exercises

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,8 @@ const App = () => {
     parts: [
       { name: "Fundamentals of React", exercises: 10, id: 1 },
       { name: "Using props to pass data", exercises: 7, id: 2 },
-      { name: "State of a component", exercises: 14, id: 3 }
+      { name: "State of a component", exercises: 14, id: 3 },
+      { name: "Redux", exercises: 11, id: 4 }
     ]
   }
 
--- src/components/Total.jsx
+++ src/components/Total.jsx
@@ -1,6 +1,6 @@
 const Total = ({ parts }) => {
     const total = parts.reduce((accum, current) => accum + current.exercises, 0);
-    return <p>{total}</p>
+    return <p>total of {total} exercises</p>
 }
 
 export default Total;
