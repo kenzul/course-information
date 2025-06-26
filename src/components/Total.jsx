2b59a514bb5764e5229399c79aed46119754249e
Jean Alvarez <blackraisondetre126@gmail.com>
2025-06-26 09:26:11 -0500

    Allow for multiple courses to be rendered

--- src/components/Total.jsx
+++ src/components/Total.jsx
@@ -1,6 +1,6 @@
 const Total = ({ parts }) => {
     const total = parts.reduce((accum, current) => accum + current.exercises, 0);
-    return <p>total of {total} exercises</p>
+    return <p><strong>total of {total} exercises</strong></p>
 }
 
 export default Total;
